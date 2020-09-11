6e153376d38f7fa0604bb1427ca58f66e7232724
unknown <vovaslesarev@gmail.com>
2020-09-11 14:23:31 +0300

    Updates

---
+++ precache-manifest.933a0648f965899b7cb06980f817992f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ec782120f96d4fa5be45b91a6af2b6a",
+    "url": "/amazonclone/index.html"
+  },
+  {
+    "revision": "d21cac07ca453ca396bd",
+    "url": "/amazonclone/static/css/main.c710a9fc.chunk.css"
+  },
+  {
+    "revision": "f64d71dcf91ef46987a8",
+    "url": "/amazonclone/static/js/2.ec8af7cb.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/amazonclone/static/js/2.ec8af7cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d21cac07ca453ca396bd",
+    "url": "/amazonclone/static/js/main.df300e5a.chunk.js"
+  },
+  {
+    "revision": "ffd49b80eeeb277c386c",
+    "url": "/amazonclone/static/js/runtime-main.cb18d772.js"
+  }
+]);
